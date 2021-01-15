0e579da55bd773a32d8a7cc08b4b70fd05a30c94
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2021-01-14 22:42:12 -0300

    relacionamento entre os models

--- src/database/migrations/1606909455160-CreateAppointments.ts
+++ src/database/migrations/1606909455160-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1606909455160
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
