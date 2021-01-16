213b1eafdb18444889372381be517ac7cde1b39c
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2021-01-16 11:50:55 -0300

    Gerando o token JWT para autenticar o user

--- src/routes/sessions.routes.ts
+++ src/routes/sessions.routes.ts
@@ -10,12 +10,12 @@ sessionsRouter.post('/', async (request, response) => {
 
     const authenticateUser = new AuthenticateUserService();
 
-    const { user } = await authenticateUser.execute({ email, password });
+    const { user, token } = await authenticateUser.execute({ email, password });
 
     // @ts-expect-error
     delete user.password;
 
-    return response.json({ user });
+    return response.json({ user, token });
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
